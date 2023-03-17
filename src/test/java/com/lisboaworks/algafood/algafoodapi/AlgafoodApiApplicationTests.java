71c223172fc43901bd07d48b3f223e830eec3baf
NatanLisboa <natanflisboa1@gmail.com>
2023-03-17 18:29:41 -0300

    Add project basic structure

---
+++ src/test/java/com/lisboaworks/algafood/algafoodapi/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisboaworks.algafood.algafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
