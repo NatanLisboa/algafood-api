044fd6e7c0ff9897568e1190f954f941abb5bbcc
NatanLisboa <natanflisboa1@gmail.com>
2023-03-24 13:31:31 -0300

    Fix CustomerActivationService bean instantiation

--- src/main/java/com/lisboaworks/algafood/algafoodapi/service/CustomerActivationService.java
+++ src/main/java/com/lisboaworks/algafood/algafoodapi/service/CustomerActivationService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerActivationService {
 
     @Autowired
