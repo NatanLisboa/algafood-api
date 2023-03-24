f2c529c808d5102ea879d1a96172dbd9dfce0c50
NatanLisboa <natanflisboa1@gmail.com>
2023-03-24 13:54:41 -0300

    Create NotificationProperties with ConfigurationProperties annotation

--- src/main/java/com/lisboaworks/algafood/algafoodapi/notification/EmailNotifier.java
+++ src/main/java/com/lisboaworks/algafood/algafoodapi/notification/EmailNotifier.java
@@ -1,7 +1,7 @@
 package com.lisboaworks.algafood.algafoodapi.notification;
 
 import com.lisboaworks.algafood.algafoodapi.model.Customer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +10,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailNotifier implements Notifier {
 
-    @Value("${notifier.email.host-server}")
-    private String host;
+//    @Value("${notifier.email.host-server}")
+//    private String host;
+//
+//    @Value("${notifier.email.server-port}")
+//    private Integer port;
 
-    @Value("${notifier.email.server-port}")
-    private Integer port;
+    @Autowired
+    private NotifierProperties properties;
 
     public EmailNotifier() {
         System.out.println("REAL EmailNotifier");
     }
 
     public void notify(Customer customer, String message) {
-        System.out.println("Host: " + host);
-        System.out.println("Port: " + port);
+        System.out.println("Host: " + properties.getServerHost());
+        System.out.println("Port: " + properties.getServerPort());
 
 
         System.out.printf("Notifying %s by email %s: %s\n",
