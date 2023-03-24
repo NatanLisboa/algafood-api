555350d4a1cb65129995067fd5c83fcebe4b5bf1
NatanLisboa <natanflisboa1@gmail.com>
2023-03-24 15:56:02 -0300

    Create others .properties files according to Spring profiles

--- src/main/java/com/lisboaworks/algafood/algafoodapi/notification/EmailNotifier.java
+++ src/main/java/com/lisboaworks/algafood/algafoodapi/notification/EmailNotifier.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
+//@Profile("prod")
 @NotifierType(UrgencyLevel.NO_URGENT)
 @Component
 public class EmailNotifier implements Notifier {
