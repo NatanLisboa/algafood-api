9a59e41e523a4b8ad6a3ef5c8c537dcf4dff4adc
NatanLisboa <natanflisboa1@gmail.com>
2023-04-03 15:26:07 -0300

    Implement method to get singleton resource of Cuisine

--- src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
@@ -3,8 +3,8 @@ package com.lisboaworks.algafood.api.controller;
 import com.lisboaworks.algafood.domain.model.Cuisine;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +20,11 @@ public class CuisineController {
     @GetMapping
     public List<Cuisine> findAll() {
         return cuisineRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cuisine findById(@PathVariable Long id) {
+        return cuisineRepository.findById(id);
     }
 
 }
