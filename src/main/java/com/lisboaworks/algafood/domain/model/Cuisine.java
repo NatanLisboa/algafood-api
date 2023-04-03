9b2810d7009fd52b56672299cb034c967d394313
NatanLisboa <natanflisboa1@gmail.com>
2023-04-03 17:45:32 -0300

    Customize XML presentation with JsonProperty, JsonIgnore and JsonRootName annotations

--- src/main/java/com/lisboaworks/algafood/domain/model/Cuisine.java
+++ src/main/java/com/lisboaworks/algafood/domain/model/Cuisine.java
@@ -1,10 +1,14 @@
 package com.lisboaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
+@JsonRootName("cuisines")
 @Entity
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -15,6 +19,9 @@ public class Cuisine {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
+    //@JsonProperty("title")
+    @Column(nullable = false)
     private String name;
 
 }
