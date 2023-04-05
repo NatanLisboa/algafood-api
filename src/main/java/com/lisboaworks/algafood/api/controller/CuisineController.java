7f44dc01d417ecb6526bc22bf215f1b692d3e10f
NatanLisboa <natanflisboa1@gmail.com>
2023-04-05 13:45:53 -0300

    Implement POST method in CuisineController

--- src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
@@ -4,7 +4,6 @@ import com.lisboaworks.algafood.api.model.CuisineXMLWrapper;
 import com.lisboaworks.algafood.domain.model.Cuisine;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -40,6 +39,12 @@ public class CuisineController {
 
         return ResponseEntity.ok(cuisine);
 
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cuisine add(@RequestBody Cuisine cuisine) {
+        return cuisineRepository.save(cuisine);
     }
 
 }
