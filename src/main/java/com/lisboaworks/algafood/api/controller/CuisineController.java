7c34ba1c7075487d9fb577f9c7a7a663f2be19f2
NatanLisboa <natanflisboa1@gmail.com>
2023-04-05 14:17:31 -0300

    Implement DELETE mapping in CuisineController

--- src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/CuisineController.java
@@ -5,6 +5,7 @@ import com.lisboaworks.algafood.domain.model.Cuisine;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -60,6 +61,23 @@ public class CuisineController {
         BeanUtils.copyProperties(newCuisine, cuisine, "id");
 
         return ResponseEntity.ok(cuisineRepository.save(cuisine));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cuisine> delete(@PathVariable Long id) {
+        try {
+            Cuisine cuisine = cuisineRepository.findById(id);
+
+            if (Objects.isNull(cuisine)) {
+                return ResponseEntity.notFound().build();
+            }
+
+            cuisineRepository.delete(cuisine);
+            return ResponseEntity.noContent().build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
     }
 
 }
