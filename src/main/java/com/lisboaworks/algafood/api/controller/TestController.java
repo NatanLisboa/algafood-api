61fb0a02a39349c3872d4868a40ea11a8855a978
NatanLisboa <natanflisboa1@gmail.com>
2023-04-12 18:23:54 -0300

    Test query methods in Spring Data JPA

--- src/main/java/com/lisboaworks/algafood/api/controller/TestController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.lisboaworks.algafood.api.controller;
 
+import com.lisboaworks.algafood.domain.model.Cuisine;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/test")
@@ -12,8 +18,13 @@ public class TestController {
     @Autowired
     private CuisineRepository cuisineRepository;
 
-//    @GetMapping("/cuisines")
-//    public List<Cuisine> getCuisinesByName(@RequestParam("name") String name) {
-//        return cuisineRepository.findByName(name);
-//    }
+    @GetMapping("/cuisines")
+    public List<Cuisine> getCuisinesByName(@RequestParam String name) {
+        return cuisineRepository.findCuisinesByName(name);
+    }
+
+    @GetMapping("/cuisine")
+    public Optional<Cuisine> getCuisineByName(@RequestParam String name) {
+        return cuisineRepository.findByName(name);
+    }
 }
--- src/main/java/com/lisboaworks/algafood/domain/repository/CuisineRepository.java
+++ src/main/java/com/lisboaworks/algafood/domain/repository/CuisineRepository.java
@@ -4,10 +4,15 @@ import com.lisboaworks.algafood.domain.model.Cuisine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CuisineRepository extends JpaRepository<Cuisine, Long> {
 
-//    List<Cuisine> findByName(String name);
+    List<Cuisine> findCuisinesByName(String name);
+
+    Optional<Cuisine> findByName(String name);
 
 
 }
