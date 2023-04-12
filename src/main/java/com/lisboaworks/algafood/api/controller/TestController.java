a6a745fb5b4a4f339bb15994c7bce068dedd4362
NatanLisboa <natanflisboa1@gmail.com>
2023-04-12 19:15:39 -0300

    Test query method with between clause

--- src/main/java/com/lisboaworks/algafood/api/controller/TestController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.lisboaworks.algafood.api.controller;
 
 import com.lisboaworks.algafood.domain.model.Cuisine;
+import com.lisboaworks.algafood.domain.model.Restaurant;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
+import com.lisboaworks.algafood.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +21,9 @@ public class TestController {
     @Autowired
     private CuisineRepository cuisineRepository;
 
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
     @GetMapping("/cuisines")
     public List<Cuisine> getCuisinesByName(@RequestParam String name) {
         return cuisineRepository.findCuisinesByNameContaining(name);
@@ -27,4 +33,11 @@ public class TestController {
     public Optional<Cuisine> getCuisineByName(@RequestParam String name) {
         return cuisineRepository.findByName(name);
     }
+
+    @GetMapping("/restaurants-by-shipping-fee")
+    public List<Restaurant> getRestaurantsByShippingFee(BigDecimal startFee, BigDecimal endFee) {
+        return restaurantRepository.findByShippingFeeBetween(startFee, endFee);
+    }
+
+
 }
--- src/main/java/com/lisboaworks/algafood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/lisboaworks/algafood/domain/repository/RestaurantRepository.java
@@ -3,6 +3,11 @@ package com.lisboaworks.algafood.domain.repository;
 import com.lisboaworks.algafood.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    List<Restaurant> findByShippingFeeBetween(BigDecimal startFee, BigDecimal endFee);
 
 }
