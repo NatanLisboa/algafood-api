b9202f538bebfebb0cb313376a56e3af6965bcf9
NatanLisboa <natanflisboa1@gmail.com>
2023-04-12 15:30:57 -0300

    Refactor Role repository in order to use Spring Data JPA

--- src/main/java/com/lisboaworks/algafood/domain/repository/RoleRepository.java
+++ src/main/java/com/lisboaworks/algafood/domain/repository/RoleRepository.java
@@ -1,18 +1,10 @@
 package com.lisboaworks.algafood.domain.repository;
 
 import com.lisboaworks.algafood.domain.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RoleRepository {
-
-
-    List<Role> findAll();
-
-    Role findById(Long id);
-
-    Role save(Role role);
-
-    void delete(Role role);
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
