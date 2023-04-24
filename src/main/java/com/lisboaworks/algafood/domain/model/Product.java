e4cf1b794bcde0eb4c368deb2de3f034e43cb177
NatanLisboa <natanflisboa1@gmail.com>
2023-04-24 14:28:12 -0300

    Map many-to-one relationship between Product and Restaurant by Product side

---
+++ src/main/java/com/lisboaworks/algafood/domain/model/Product.java
@@ -0,0 +1,34 @@
+package com.lisboaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurant restaurant;
+
+}
