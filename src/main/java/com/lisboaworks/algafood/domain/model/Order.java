83601ae13dd4515d244c3bf9d818e4b93e594cd1
NatanLisboa <natanflisboa1@gmail.com>
2023-05-05 19:30:15 -0300

    7.12 Map Order entity

---
+++ src/main/java/com/lisboaworks/algafood/domain/model/Order.java
@@ -0,0 +1,39 @@
+package com.lisboaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false)
+    private BigDecimal shippingFee;
+
+    @Column(nullable = false)
+    private BigDecimal totalValue;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDatetime;
+
+    private LocalDateTime confirmationDatetime;
+
+    private LocalDateTime cancellationDatetime;
+
+    private LocalDateTime deliveryDatetime;
+}
