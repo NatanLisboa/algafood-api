2ba2fee9cf082680a6ea40bbd610a25a31e60008
NatanLisboa <natanflisboa1@gmail.com>
2023-05-12 19:40:33 -0300

    Handle global exceptions with @ExceptionHandler and @ControllerAdvice

--- src/main/java/com/lisboaworks/algafood/api/controller/CityController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/CityController.java
@@ -1,10 +1,7 @@
 package com.lisboaworks.algafood.api.controller;
 
 
-import com.lisboaworks.algafood.api.exceptionhandler.ApiException;
 import com.lisboaworks.algafood.domain.exception.BusinessRuleException;
-import com.lisboaworks.algafood.domain.exception.EntityAlreadyInUseException;
-import com.lisboaworks.algafood.domain.exception.EntityNotFoundException;
 import com.lisboaworks.algafood.domain.exception.StateNotFoundException;
 import com.lisboaworks.algafood.domain.model.City;
 import com.lisboaworks.algafood.domain.repository.CityRepository;
@@ -12,12 +9,9 @@ import com.lisboaworks.algafood.domain.service.CityRegisterService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/cities")
@@ -68,28 +62,5 @@ public class CityController {
     public void delete(@PathVariable Long id) {
         cityRegisterService.delete(id);
     }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
-        ApiException apiException = ApiException.builder()
-                .datetime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(apiException);
-    }
-
-    @ExceptionHandler(BusinessRuleException.class)
-    public ResponseEntity<?> handleBusinessRuleException(EntityNotFoundException e) {
-        ApiException apiException = ApiException.builder()
-                .datetime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(apiException);
-    }
-
 
 }
---
+++ src/main/java/com/lisboaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.lisboaworks.algafood.api.exceptionhandler;
+
+import com.lisboaworks.algafood.domain.exception.BusinessRuleException;
+import com.lisboaworks.algafood.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        ApiException apiException = ApiException.builder()
+                .datetime(LocalDateTime.now())
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(apiException);
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<?> handleBusinessRuleException(EntityNotFoundException e) {
+        ApiException apiException = ApiException.builder()
+                .datetime(LocalDateTime.now())
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(apiException);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> handleHttpMediaTypeNotSupportedException() {
+        ApiException apiException = ApiException.builder()
+                .datetime(LocalDateTime.now())
+                .message("This media type is not accepted. The API supports only JSON media type")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(apiException);
+    }
+
+}
