5f898c5186b8493b039fed9d31763dc683772330
NatanLisboa <natanflisboa1@gmail.com>
2023-05-15 15:18:15 -0300

    Customize default response body of ResponseEntityExceptionHandler

--- src/main/java/com/lisboaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/lisboaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,49 +3,51 @@ package com.lisboaworks.algafood.api.exceptionhandler;
 import com.lisboaworks.algafood.domain.exception.BusinessRuleException;
 import com.lisboaworks.algafood.domain.exception.EntityAlreadyInUseException;
 import com.lisboaworks.algafood.domain.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
-        ApiException apiException = ApiException.builder()
-                .datetime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(apiException);
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e, WebRequest request /* Automatically injected by Spring */) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(BusinessRuleException.class)
-    public ResponseEntity<?> handleBusinessRuleException(BusinessRuleException e) {
-        ApiException apiException = ApiException.builder()
-                .datetime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(apiException);
+    public ResponseEntity<?> handleBusinessRuleException(BusinessRuleException e, WebRequest request /* Automatically injected by Spring */) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(EntityAlreadyInUseException.class)
-    public ResponseEntity<?> handleEntityAlreadyInUseException(EntityAlreadyInUseException e) {
-        ApiException apiException = ApiException.builder()
-                .datetime(LocalDateTime.now())
-                .message(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(apiException);
+    public ResponseEntity<?> handleEntityAlreadyInUseException(EntityAlreadyInUseException e, WebRequest request /* Automatically injected by Spring */ ) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (Objects.isNull(body)) {
+            body = ApiException.builder()
+                    .datetime(LocalDateTime.now())
+                    .message(status.getReasonPhrase())
+                    .build();
+        } else if (body instanceof String) {
+            body = ApiException.builder()
+                    .datetime(LocalDateTime.now())
+                    .message((String) body)
+                    .build();
+        }
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 }
