84b841ef4f63c63d524aabac605ede441caaea6a
NatanLisboa <natanflisboa1@gmail.com>
2023-05-17 17:59:58 -0300

    Fix: Throw deserialization exception on Restaurant partially update

--- src/main/java/com/lisboaworks/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/lisboaworks/algafood/api/controller/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.lisboaworks.algafood.api.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lisboaworks.algafood.domain.exception.BusinessRuleException;
 import com.lisboaworks.algafood.domain.exception.CuisineNotFoundException;
@@ -7,12 +8,16 @@ import com.lisboaworks.algafood.domain.model.Restaurant;
 import com.lisboaworks.algafood.domain.repository.CuisineRepository;
 import com.lisboaworks.algafood.domain.repository.RestaurantRepository;
 import com.lisboaworks.algafood.domain.service.RestaurantRegisterService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -64,23 +69,35 @@ public class RestaurantController {
     }
 
     @PatchMapping("/{id}")
-    public Restaurant partiallyUpdate(@PathVariable Long id, @RequestBody Map<String, Object> fieldsToUpdate) {
+    public Restaurant partiallyUpdate(@PathVariable Long id, @RequestBody Map<String, Object> fieldsToUpdate,
+                                      HttpServletRequest request) {
         Restaurant restaurant = restaurantRegisterService.findOrThrowException(id);
-        merge(fieldsToUpdate, restaurant);
+        merge(fieldsToUpdate, restaurant, request);
         return update(restaurant.getId(), restaurant);
     }
 
-    private void merge(Map<String, Object> sourceFields, Restaurant targetObject) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurant restaurant = objectMapper.convertValue(sourceFields, Restaurant.class);
+    private void merge(Map<String, Object> sourceFields, Restaurant targetObject, HttpServletRequest request) {
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
-        sourceFields.forEach((propertyField, propertyValue) -> {
-            Field field = ReflectionUtils.findField(Restaurant.class, propertyField);
-            assert field != null;
-            field.setAccessible(true);
-            Object newPropertyValue = ReflectionUtils.getField(field, restaurant);
-            ReflectionUtils.setField(field, targetObject, newPropertyValue);
-        });
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+
+            Restaurant restaurant = objectMapper.convertValue(sourceFields, Restaurant.class);
+
+            sourceFields.forEach((propertyField, propertyValue) -> {
+                Field field = ReflectionUtils.findField(Restaurant.class, propertyField);
+                assert field != null;
+                field.setAccessible(true);
+                Object newPropertyValue = ReflectionUtils.getField(field, restaurant);
+                ReflectionUtils.setField(field, targetObject, newPropertyValue);
+            });
+        } catch (IllegalArgumentException e) {
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+        }
+
     }
 
 }
