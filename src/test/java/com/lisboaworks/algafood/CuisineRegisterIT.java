4ccdf82a2228c0d5b0036625297e6ac8211222fb
NatanLisboa <natanflisboa1@gmail.com>
2023-05-31 15:57:06 -0300

    Revert "chore: Create method to setup tests"
    This reverts commit f0d245f8a6263b3a90274dc188cf838b1e7e4151.

--- src/test/java/com/lisboaworks/algafood/CuisineRegisterIT.java
+++ src/test/java/com/lisboaworks/algafood/CuisineRegisterIT.java
@@ -1,9 +1,9 @@
 package com.lisboaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -17,16 +17,11 @@ public class CuisineRegisterIT {
 	
 	@LocalServerPort
 	private int port;
-	
-	@BeforeEach
-	public void setup() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();		
-		RestAssured.port = port;
-		RestAssured.basePath = "/cuisines";
-	}
     
 	@Test
-	public void shouldReturnStatusOK_WhenGettingCuisines() {		
+	public void shouldReturnStatusOK_WhenGettingCuisines() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
 		given()
 			.basePath("/cuisines")
 			.port(port)
@@ -38,7 +33,9 @@ public class CuisineRegisterIT {
 	}
 	
 	@Test
-	public void shouldContainTwoCuisines_WhenGettingCuisines() {		
+	public void shouldContainTwoCuisines_WhenGettingCuisines() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
 		given()
 			.basePath("/cuisines")
 			.port(port)
@@ -46,7 +43,8 @@ public class CuisineRegisterIT {
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(2));
+			.body("", hasSize(2))
+			.body("name", hasItems("Thai", "Indian"));
 	}
 
 }
