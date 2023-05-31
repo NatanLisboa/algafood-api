40b0ddaa4d42cb41cd763e4473c89e302f05ba77
NatanLisboa <natanflisboa1@gmail.com>
2023-05-31 15:35:31 -0300

    chore: Validate HTTP response body

--- src/test/java/com/lisboaworks/algafood/CuisineRegisterIT.java
+++ src/test/java/com/lisboaworks/algafood/CuisineRegisterIT.java
@@ -1,6 +1,8 @@
 package com.lisboaworks.algafood;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +30,21 @@ public class CuisineRegisterIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void shouldContainTwoCuisines_WhenGettingCuisines() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cuisines")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(2))
+			.body("name", hasItems("Thai", "Indian"));
 	}
 
 }
