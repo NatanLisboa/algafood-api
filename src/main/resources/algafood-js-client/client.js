ad546af12a50622f6d34b110b959b52283c74b53
NatanLisboa <natanflisboa1@gmail.com>
2023-08-07 20:23:36 -0300

    feat: Implement a registration form. Make a POST request with JavaScript (doing)

--- src/main/resources/algafood-js-client/client.js
+++ src/main/resources/algafood-js-client/client.js
@@ -9,6 +9,25 @@ function getPaymentMethods() {
   });
 }
 
+function registerPaymentMethod() {
+    var paymentMethodJson = JSON.stringify({
+        "description": $("#description-field")
+    });
+
+    console.log(paymentMethodJson);
+
+    $.ajax({
+        url: "http://api.algafood.local:8080/payment-method",
+        type: "post",
+        data: paymentMethodJson,
+        contentType: "application/json"
+
+        success: function(response) {
+            alert("Payment method registered!");
+            getPaymentMethods();
+        }
+    });
+}
 
 function fillTable(paymentMethods) {
   $("#table tbody tr").remove();
@@ -24,6 +43,5 @@ function fillTable(paymentMethods) {
     row.appendTo("#table");
   });
 }
-
 
 $("#btn-get").click(getPaymentMethods);
