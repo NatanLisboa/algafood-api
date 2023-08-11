0f7a245cc5ec4ef56a2c62999f9c24eb090b366a
NatanLisboa <natanflisboa1@gmail.com>
2023-08-11 15:00:51 -0300

    fix: Describe API informations in documentation

--- src/main/java/com/lisboaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.lisboaworks.algafood.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +22,16 @@ public class SpringFoxConfig {
                     .apis(RequestHandlerSelectors.basePackage("com.lisboaworks.algafood.api"))
                     .paths(PathSelectors.any())
 //                  .paths(PathSelectors.ant("/restaurants/*"))
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("Open API to customers and restaurants")
+                .version("1")
+                .contact(new Contact("Natan da Fonseca Lisboa", "https://github.com/NatanLisboa", "natanflisboa1@gmail.com"))
                 .build();
     }
 
