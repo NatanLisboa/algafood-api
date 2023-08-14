8dfb80a463c4df78f17264602a75887eae0d924c
NatanLisboa <natanflisboa1@gmail.com>
2023-08-14 15:15:38 -0300

    docs: Reference api exception representation template with error status codes

--- src/main/java/com/lisboaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
+import org.springframework.http.MediaType;
+import springfox.documentation.builders.*;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
@@ -20,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 @Configuration
 @EnableSwagger2
@@ -37,12 +36,63 @@ public class SpringFoxConfig {
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponses(HttpMethod.GET, this.globalGetResponseMessages())
-                .globalResponses(HttpMethod.POST, this.globalPostResponseMessages())
-                .globalResponses(HttpMethod.PUT, this.globalPutResponseMessages())
+                .globalResponses(HttpMethod.POST, this.globalPostPutResponseMessages())
+                .globalResponses(HttpMethod.PUT, this.globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, this.globalDeleteResponseMessages())
                 .additionalModels(typeResolver.resolve(ApiException.class))
                 .apiInfo(this.apiInfo())
                 .tags(new Tag("Cities", "Manage the cities"));
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Algafood API")
+                .description("Open API to customers and restaurants")
+                .version("1")
+                .contact(new Contact("Natan da Fonseca Lisboa", "https://github.com/NatanLisboa", "natanflisboa1@gmail.com"))
+                .build();
+    }
+
+    private List<Response> globalGetResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))
+                        .description("Internal server error")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(this.apiExceptionBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE))
+                        .description("Resource has no representation that can be accepted by the consumer")
+                        .build()
+        );
+    }
+
+    private List<Response> globalPostPutResponseMessages() {
+        return Arrays.asList(
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.BAD_REQUEST))
+                        .description("Bad request (client error)")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(this.apiExceptionBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))
+                        .description("Internal server error")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(this.apiExceptionBuilder())
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE))
+                        .description("Resource has no representation that can be accepted by the consumer")
+                        .build(),
+                new ResponseBuilder()
+                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE))
+                        .description("Request denied because the body is in an unsupported format")
+                        .representation(MediaType.APPLICATION_JSON)
+                        .apply(this.apiExceptionBuilder())
+                        .build()
+        );
     }
 
     private List<Response> globalDeleteResponseMessages() {
@@ -58,68 +108,14 @@ public class SpringFoxConfig {
         );
     }
 
-    private List<Response> globalPutResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.BAD_REQUEST))
-                        .description("Bad request (client error)")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))
-                        .description("Internal server error")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE))
-                        .description("Resource has no representation that can be accepted by the consumer")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE))
-                        .description("Request denied because the body is in an unsupported format")
-                        .build()
-        );
-    }
-
-    private List<Response> globalPostResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.BAD_REQUEST))
-                        .description("Bad request (client error)")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))
-                        .description("Internal server error")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE))
-                        .description("Resource has no representation that can be accepted by the consumer")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE))
-                        .description("Request denied because the body is in an unsupported format")
-                        .build()
-        );
-    }
-
-    private List<Response> globalGetResponseMessages() {
-        return Arrays.asList(
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR))
-                        .description("Internal server error")
-                        .build(),
-                new ResponseBuilder()
-                        .code(String.valueOf(HttpStatus.NOT_ACCEPTABLE))
-                        .description("Resource has no representation that can be accepted by the consumer")
-                        .build()
-        );
-    }
-
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Algafood API")
-                .description("Open API to customers and restaurants")
-                .version("1")
-                .contact(new Contact("Natan da Fonseca Lisboa", "https://github.com/NatanLisboa", "natanflisboa1@gmail.com"))
-                .build();
+    private Consumer<RepresentationBuilder> apiExceptionBuilder() {
+        return r -> r.model(m -> m.name("ApiException")
+                .referenceModel(
+                        ref -> ref.key(
+                                k -> k.qualifiedModelName(
+                                        q -> q.name("ApiException")
+                                                .namespace("com.lisboaworks.algafood.api.exceptionhandler")
+                                ))));
     }
 
 }
