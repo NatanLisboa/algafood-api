b683fba50e09f7bff1ae71fd8938e9557e42c31d
NatanLisboa <natanflisboa1@gmail.com>
2023-09-11 20:33:11 -0300

    chore!: Zip HTTP responses with Gzip

--- src/main/java/com/lisboaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/web/WebConfig.java
@@ -1,5 +1,6 @@
 package com.lisboaworks.algafood.core.web;
 
+import com.github.ziplet.filter.compression.CompressingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
@@ -20,6 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public Filter shallowETagHeaderFilter() {
         return new ShallowEtagHeaderFilter();
+    }
+
+    @Bean
+    public Filter compressingFilter() {
+        return new CompressingFilter();
     }
 
 }
