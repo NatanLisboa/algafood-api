4e9544486d393954d756014e06df4cb92f1e3c39
NatanLisboa <natanflisboa1@gmail.com>
2023-09-12 16:44:54 -0300

    fix!: Fix the pagination in documentation for Cuisines

--- src/main/java/com/lisboaworks/algafood/api/openapi/model/CuisinesModelOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/openapi/model/CuisinesModelOpenApi.java
@@ -2,9 +2,29 @@ package com.lisboaworks.algafood.api.openapi.model;
 
 import com.lisboaworks.algafood.api.model.CuisineModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 
 @ApiModel("CuisinesModel")
-public class CuisinesModelOpenApi extends PaginatedModelOpenAPI<CuisineModel> {
+@Getter
+@Setter
+public class CuisinesModelOpenApi {
+
+    private CuisinesEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("CuisinesEmbeddedModel")
+    @Data
+    public static class CuisinesEmbeddedModelOpenApi {
+
+        private List<CuisineModel> cuisines;
+
+    }
 
 }
