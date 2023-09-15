01d843f82d47124d52189b7e274bd67cf1fd4666
NatanLisboa <natanflisboa1@gmail.com>
2023-09-15 18:47:18 -0300

    fix: Define the API default version when the Media Type is not informed

--- src/main/java/com/lisboaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/web/WebConfig.java
@@ -4,6 +4,7 @@ import com.github.ziplet.filter.compression.CompressingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,6 +22,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public Filter shallowETagHeaderFilter() {
         return new ShallowEtagHeaderFilter();
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(AlgaMediaTypes.V2_APPLICATION_JSON);
     }
 
     @Bean
