7087383614db89a919602d153722121423c0f953
NatanLisboa <natanflisboa1@gmail.com>
2023-09-18 20:07:11 -0300

    chore: Optimize imports

--- src/main/java/com/lisboaworks/algafood/api/v2/model/input/CityInputV2.java
+++ src/main/java/com/lisboaworks/algafood/api/v2/model/input/CityInputV2.java
@@ -1,12 +1,10 @@
 package com.lisboaworks.algafood.api.v2.model.input;
 
-import com.lisboaworks.algafood.api.v1.model.input.StateIdInput;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
