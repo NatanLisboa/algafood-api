625e5a01741705a36b64ea53b80c8dc06508ec66
NatanLisboa <natanflisboa1@gmail.com>
2023-09-19 12:30:09 -0300

    fix: Fix CityModel name in documentation

--- src/main/java/com/lisboaworks/algafood/api/v2/model/CityModelV2.java
+++ src/main/java/com/lisboaworks/algafood/api/v2/model/CityModelV2.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
-@ApiModel("CuisineModel")
+@ApiModel("CityModel")
 @Relation(collectionRelation = "cities")
 @Getter
 @Setter
