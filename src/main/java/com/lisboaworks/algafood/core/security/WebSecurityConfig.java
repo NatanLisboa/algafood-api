3347634a5c4da3528c1842ddb812f3cdc82de474
NatanLisboa <natanflisboa1@gmail.com>
2023-09-21 18:44:41 -0300

    config: Configure in-memory users authentication

--- src/main/java/com/lisboaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/security/WebSecurityConfig.java
@@ -5,7 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +33,27 @@ public class WebSecurityConfig {
                 .csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.builder()
+                .username("algafood")
+                .password(this.passwordEncoder().encode("123456"))
+                .roles("ADMIN")
+                .build());
+        userDetailsList.add(User.builder()
+                .username("algafood-2")
+                .password(this.passwordEncoder().encode("123456"))
+                .roles("ADMIN")
+                .build());
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
