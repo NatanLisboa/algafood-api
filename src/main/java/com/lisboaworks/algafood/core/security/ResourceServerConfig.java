9e4f6d5555866875652b26bc9c1168701cd19a83
NatanLisboa <natanflisboa1@gmail.com>
2023-09-24 16:42:21 -0300

    config: Configure Resource Server with new Spring Security stack

---
+++ src/main/java/com/lisboaworks/algafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,24 @@
+package com.lisboaworks.algafood.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer().opaqueToken();
+
+        return http.build();
+    }
+
+}
