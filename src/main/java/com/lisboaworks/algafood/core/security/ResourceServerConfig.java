cece7a4f8977b5da32fc492aa05343bbc5f5fc1c
NatanLisboa <natanflisboa1@gmail.com>
2023-10-06 14:00:26 -0300

    config: Configure the Resource Server to JWT signed with symmetric key

--- src/main/java/com/lisboaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/security/ResourceServerConfig.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -17,9 +22,16 @@ public class ResourceServerConfig {
                 .anyRequest().authenticated()
             .and()
             .cors().and()
-            .oauth2ResourceServer().opaqueToken();
+            .oauth2ResourceServer().jwt();
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 
 }
