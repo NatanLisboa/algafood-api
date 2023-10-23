8c03d2ce659d532db92b638bd914220aeaf64ee2
NatanLisboa <natanflisboa1@gmail.com>
2023-10-23 15:51:43 -0300

    config!: Register OAuth2 clients in the database and test token issuance

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -18,6 +18,7 @@ delete from restaurant_responsible_user;
 delete from `order`;
 delete from order_item;
 delete from product_photo;
+delete from oauth_client_details;
 
 -- Enable foreign key checks again
 set foreign_key_checks = 1;
@@ -213,3 +214,36 @@ values (9, 'dfd70411-ee92-487a-b41a-88d5de02a002', 4, 5, 1, 1, '38400-111', 'St 
 
 insert into order_item (id, order_id, product_id, quantity, unit_price, total_price, note)
 values (13, 9, 6, 3, 15.00, 45.00, null);
+
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret,
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'algafood-web', null, '$2y$12$w3igMjsfS5XoAYuowoH3C.54vRFWlcXSHLjX7MwF990Kc2KKKh72e',
+  'READ,WRITE', 'password', null, null,
+  60 * 60 * 6, 60 * 24 * 60 * 60, null
+);
+
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret,
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'food-analytics', null, '$2y$12$fahbH37S2pyk1RPuIHKP.earzFmgAJJGo26rE.59vf4wwiiTKHnzO',
+  'READ,WRITE', 'authorization_code', 'http://www.foodanalytics.local:8082', null,
+  null, null, null
+);
+
+insert into oauth_client_details (
+  client_id, resource_ids, client_secret,
+  scope, authorized_grant_types, web_server_redirect_uri, authorities,
+  access_token_validity, refresh_token_validity, autoapprove
+)
+values (
+  'invoicing', null, '$2a$12$yKVluj7GLkD/oE5kDWaXHOtm5DNKU9ffCR7/FUumLFGNKDCXPIp62',
+  'READ,WRITE', 'client_credentials', null, 'GET_ORDERS,GENERATE_REPORTS',
+  null, null, null
+);
