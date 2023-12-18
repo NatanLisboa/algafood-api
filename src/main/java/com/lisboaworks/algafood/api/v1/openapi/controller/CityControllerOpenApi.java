8a5eeb9dcbe3ea7866dfe1e933d43beef564676f
NatanLisboa <natanflisboa1@gmail.com>
2023-12-18 18:41:10 -0300

    docs: Describe endpoints operation in the documentation

--- src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/CityControllerOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/CityControllerOpenApi.java
@@ -2,6 +2,7 @@ package com.lisboaworks.algafood.api.v1.openapi.controller;
 
 import com.lisboaworks.algafood.api.v1.model.CityModel;
 import com.lisboaworks.algafood.api.v1.model.input.CityInput;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
@@ -10,15 +11,19 @@ import org.springframework.hateoas.CollectionModel;
 @Tag(name = "Cities")
 public interface CityControllerOpenApi {
 
+    @Operation(summary = "Get all the cities")
     CollectionModel<CityModel> findAll();
 
+    @Operation(summary = "Get a city by its id")
     CityModel findById(Long cityId);
 
-
+    @Operation(summary = "Register a city", description = "Register of a city. Needs a valid state and name")
     CityModel add(CityInput cityInput);
 
+    @Operation(summary = "Update a city by its id")
     CityModel update(Long cityId, CityInput newCityInput);
 
+    @Operation(summary = "Delete a city by its id")
     void delete(Long cityId);
 
 }
