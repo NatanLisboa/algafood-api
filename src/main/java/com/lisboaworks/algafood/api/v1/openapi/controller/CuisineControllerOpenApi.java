8ab031ec3bb32790229f0ec94b25665702d32882
NatanLisboa <natanflisboa1@gmail.com>
2024-01-08 19:13:11 -0300

    docs: Fix documentation with Pageable replacement

--- src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/CuisineControllerOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/CuisineControllerOpenApi.java
@@ -2,6 +2,11 @@ package com.lisboaworks.algafood.api.v1.openapi.controller;
 
 import com.lisboaworks.algafood.api.v1.model.CuisineModel;
 import com.lisboaworks.algafood.api.v1.model.input.CuisineInput;
+import com.lisboaworks.algafood.core.springdoc.PageableParameter;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
@@ -9,7 +14,8 @@ import org.springframework.hateoas.PagedModel;
 @SecurityRequirement(name = "security_auth")
 public interface CuisineControllerOpenApi {
 
-    PagedModel<CuisineModel> findAll(Pageable pageable);
+    @PageableParameter
+    PagedModel<CuisineModel> findAll(@Parameter(hidden = true) Pageable pageable);
 
     CuisineModel findById(Long cuisineId);
 
---
+++ src/main/java/com/lisboaworks/algafood/core/springdoc/PageableParameter.java
@@ -0,0 +1,37 @@
+package com.lisboaworks.algafood.core.springdoc;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "page",
+        description = "Page number",
+        schema = @Schema(type = "integer", defaultValue = "0")
+)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "size",
+        description = "Page size",
+        schema = @Schema(type = "integer", defaultValue = "5")
+)
+@Parameter(
+        in = ParameterIn.QUERY,
+        name = "sort",
+        description = "Sorting criteria",
+        examples = {
+                @ExampleObject("name"),
+                @ExampleObject("name,asc"),
+                @ExampleObject("name,desc")
+        }
+)
+public @interface PageableParameter { }
