ac5152b0dc041746acd6371fbe0b87cd6a6bec61
NatanLisboa <natanflisboa1@gmail.com>
2024-01-09 18:53:29 -0300

    docs: Describe documentation for orders endpoints (doing)

--- src/main/java/com/lisboaworks/algafood/api/v1/model/OrderSummaryModel.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/model/OrderSummaryModel.java
@@ -1,6 +1,7 @@
 package com.lisboaworks.algafood.api.v1.model;
 
 import com.lisboaworks.algafood.domain.model.OrderStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -14,19 +15,19 @@ import java.time.OffsetDateTime;
 @Setter
 public class OrderSummaryModel extends RepresentationModel<OrderSummaryModel> {
 
-
+    @Schema(example = "522be832-c93b-4164-a390-e62114e6177d")
     private String code;
 
-
+    @Schema(example = "63.70")
     private BigDecimal subtotal;
 
-
+    @Schema(example = "10.00")
     private BigDecimal shippingFee;
 
-
+    @Schema(example = "73.70")
     private BigDecimal totalValue;
 
-
+    @Schema(example = "2023-07-08T13:00:00Z")
     private OffsetDateTime creationDatetime;
 
     private RestaurantOnlyNameModel restaurant;
--- src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/OrderControllerOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/OrderControllerOpenApi.java
@@ -4,17 +4,52 @@ import com.lisboaworks.algafood.api.v1.model.OrderModel;
 import com.lisboaworks.algafood.api.v1.model.OrderSummaryModel;
 import com.lisboaworks.algafood.api.v1.model.input.OrderInput;
 import com.lisboaworks.algafood.domain.filter.OrderFilter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Orders")
 public interface OrderControllerOpenApi {
 
-    PagedModel<OrderSummaryModel> findAll(OrderFilter filter, Pageable pageable);
+    @Operation(summary = "Get all the orders", parameters = {
+            @Parameter(in = ParameterIn.QUERY, name = "customerId",
+                    description = "Customer id for search filter",
+                    example = "1", schema = @Schema(type = "integer")),
+            @Parameter(in = ParameterIn.QUERY, name = "restaurantId",
+                    description = "Restaurant id for search filter",
+                    example = "1", schema = @Schema(type = "integer")),
+            @Parameter(in = ParameterIn.QUERY, name = "startCreationDatetime",
+                    description = "Initial creation datetime for search filter",
+                    example = "2023-01-01T00:00:00Z", schema = @Schema(type = "string", format = "date-time")),
+            @Parameter(in = ParameterIn.QUERY, name = "endCreationDatetime",
+                    description = "Final creation datetime for search filter",
+                    example = "2023-12-31T23:59:59Z", schema = @Schema(type = "string", format = "date-time"))
+    })
+    PagedModel<OrderSummaryModel> findAll(@Parameter(hidden = true) OrderFilter filter,
+                                          @Parameter(hidden = true) Pageable pageable);
 
-    OrderModel findById(String orderCode);
+    @Operation(summary = "Get an order by its id", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "Invalid order id",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            ),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    OrderModel findById(@Parameter(description = "Cuisine id", example = "1", required = true) String orderCode);
 
+    @Operation(summary = "Issue an order", responses = {
+            @ApiResponse(responseCode = "201", description = "Order issued successfully")
+    })
     OrderModel issue(OrderInput orderInput);
 
 }
