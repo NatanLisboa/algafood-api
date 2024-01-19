446b32cf1fcc1ffac5168281aec69177089e1363
NatanLisboa <natanflisboa1@gmail.com>
2024-01-19 18:56:58 -0300

    docs: Describe documentation for association between restaurants and users

--- src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/RestaurantResponsibleUserControllerOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/RestaurantResponsibleUserControllerOpenApi.java
@@ -1,17 +1,48 @@
 package com.lisboaworks.algafood.api.v1.openapi.controller;
 
 import com.lisboaworks.algafood.api.v1.model.UserModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "Restaurants")
 public interface RestaurantResponsibleUserControllerOpenApi {
 
-    CollectionModel<UserModel> getAllResponsibleUsers(Long restaurantId);
+    @Operation(summary = "Get all the responsible users from a restaurant", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "Restaurant not found",
+                    content = @Content(schema = @Schema(ref = "ApiException")))
+    })
+    CollectionModel<UserModel> getAllResponsibleUsers(@Parameter(description = "Restaurant id", example = "1", required = true) 
+                                                      Long restaurantId);
 
-    ResponseEntity<Void> associateResponsibleUser(Long restaurantId, Long userId);
+    @Operation(summary = "Associate responsible user to a restaurant", responses = {
+            @ApiResponse(responseCode = "204", description = "User associated successfully to restaurant"),
+            @ApiResponse(responseCode = "404", description = "User or restaurant not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    ResponseEntity<Void> associateResponsibleUser(@Parameter(description = "Restaurant id", example = "1", required = true)
+                                                  Long restaurantId,
+                                                  @Parameter(description = "User id", example = "1", required = true) 
+                                                  Long userId);
 
-    ResponseEntity<Void> disassociateResponsibleUser(Long restaurantId, Long userId);
+    @Operation(summary = "Disassociate responsible user from a restaurant", responses = {
+            @ApiResponse(responseCode = "204", description = "User disassociated successfully from restaurant"),
+            @ApiResponse(responseCode = "404", description = "User or restaurant not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    ResponseEntity<Void> disassociateResponsibleUser(@Parameter(description = "Restaurant id", example = "1", required = true) 
+                                                     Long restaurantId,
+                                                     @Parameter(description = "User id", example = "1", required = true)
+                                                     Long userId);
 
 }
