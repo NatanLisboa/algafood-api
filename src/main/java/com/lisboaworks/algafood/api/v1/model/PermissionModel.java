2f5448726b24d615dc3cc16e71f7fd282a5212f3
NatanLisboa <natanflisboa1@gmail.com>
2024-01-22 19:39:23 -0300

    docs: Describe documentation for association endpoints from permissions with user groups

--- src/main/java/com/lisboaworks/algafood/api/v1/model/PermissionModel.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/model/PermissionModel.java
@@ -1,6 +1,6 @@
 package com.lisboaworks.algafood.api.v1.model;
 
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
@@ -11,13 +11,13 @@ import org.springframework.hateoas.server.core.Relation;
 @Setter
 public class PermissionModel extends RepresentationModel<PermissionModel> {
 
-
+    @Schema(example = "1")
     private Long id;
 
-
+    @Schema(example = "EDIT_CUISINES")
     private String name;
 
-
+    @Schema(example = "Allow edit cuisines")
     private String description;
 
 }
--- src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/UserGroupPermissionControllerOpenApi.java
+++ src/main/java/com/lisboaworks/algafood/api/v1/openapi/controller/UserGroupPermissionControllerOpenApi.java
@@ -1,17 +1,52 @@
 package com.lisboaworks.algafood.api.v1.openapi.controller;
 
 import com.lisboaworks.algafood.api.v1.model.PermissionModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 
 @SecurityRequirement(name = "security_auth")
+@Tag(name = "User groups")
 public interface UserGroupPermissionControllerOpenApi {
 
-    CollectionModel<PermissionModel> findAll(Long userGroupId);
+    @Operation(summary = "Get all permissions from an user group", responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "Invalid user group id",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            ),
+            @ApiResponse(responseCode = "404", description = "User group not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    CollectionModel<PermissionModel> findAll(@Parameter(description = "User group id", example = "1", required = true)
+                                             Long userGroupId);
 
-    ResponseEntity<Void> associate(Long userGroupId, Long permissionId);
+    @Operation(summary = "Associate permission to an user group", responses = {
+            @ApiResponse(responseCode = "204", description = "Permission associated successfully to user group"),
+            @ApiResponse(responseCode = "404", description = "Permission or user group not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    ResponseEntity<Void> associate(@Parameter(description = "User group id", example = "1", required = true)
+                                   Long userGroupId,
+                                   @Parameter(description = "Permission id", example = "1", required = true)
+                                   Long permissionId);
 
-    ResponseEntity<Void> disassociate(Long userGroupId, Long permissionId);
+    @Operation(summary = "Disassociate permission from an user group", responses = {
+            @ApiResponse(responseCode = "204", description = "Permission disassociated successfully from user group"),
+            @ApiResponse(responseCode = "404", description = "Permission or user group not found",
+                    content = @Content(schema = @Schema(ref = "ApiException"))
+            )
+    })
+    ResponseEntity<Void> disassociate(@Parameter(description = "User group id", example = "1", required = true)
+                                      Long userGroupId,
+                                      @Parameter(description = "Permission id", example = "1", required = true)
+                                      Long permissionId);
 
 }
