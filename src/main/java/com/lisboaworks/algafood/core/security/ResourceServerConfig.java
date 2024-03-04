1c4a4ff5a5aff4b8c007881cb7cc50dfe6f47e56
NatanLisboa <natanflisboa1@gmail.com>
2024-03-04 19:31:28 -0300

    refactor: Customize the Authorization Server login page

--- src/main/java/com/lisboaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/lisboaworks/algafood/core/security/ResourceServerConfig.java
@@ -2,7 +2,6 @@ package com.lisboaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
